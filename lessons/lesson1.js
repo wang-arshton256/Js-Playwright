96514832c41699faeb6ae58b69123e97b52387bf
wang-arshton256 <wangarshton@gmail.com>
2026-02-19 12:30:39 +0300

    variables

--- lessons/lesson1.js
+++ lessons/lesson1.js
@@ -1,2 +1,18 @@
 // 1.Hello World
 console.log("Hello World");
+
+// Variables
+var firstName = "John";
+let lastName = "Smith";
+console.log(lastName);
+
+// Declaring a variable
+var age, dateOfBirth, sex;
+
+//Re assigning a vaiable a value
+age = 36;
+sex = "Male";
+console.log(age);
+age = 50;
+console.log(age);
+
