2e115af3f7df5d73bddcfca7d6227c1bb1b39632
wang-arshton256 <wangarshton@gmail.com>
2026-03-06 13:20:15 +0300

    lessons

---
+++ lessons/lesson3.js
@@ -0,0 +1,14 @@
+// Objects
+var customer = {
+    firstName: 'John',
+    lastName: 'Smith',
+}
+
+// Dot notation 
+customer['lastName'] = "Silver";
+
+// Bracket notation
+console.log(`${customer.firstName} ${customer.lastName}`);
+
+// Arrays
+var car = ["Volvo"]
